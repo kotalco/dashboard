9b3b63d4399f089ca5d44c536f197ca13b9eee29
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-09-27 11:26:58 +0300

    logs, resources for validators

--- src/app/(dashboard)/[workspaceId]/(main)/deployments/ethereum/validators/[nodeName]/page.tsx
+++ src/app/(dashboard)/[workspaceId]/(main)/deployments/ethereum/validators/[nodeName]/page.tsx
@@ -126,7 +126,7 @@ export default async function BeaconNodePage({
             <TabsContent className="px-4 py-3 sm:px-6 sm:py-4" value="logs">
               {token && (
                 <Logs
-                  url={`ethereum2/beaconnodes/${node.name}/logs?authorization=Bearer ${token}&workspace_id=${params.workspaceId}`}
+                  url={`ethereum2/validators/${node.name}/logs?authorization=Bearer ${token}&workspace_id=${params.workspaceId}`}
                 />
               )}
             </TabsContent>
@@ -138,7 +138,7 @@ export default async function BeaconNodePage({
               <ResourcesForm
                 node={node}
                 role={role}
-                updateUrl={`/ethereum2/beaconnodes/${node.name}?workspace_id=${workspaceId}`}
+                updateUrl={`/ethereum2/validators/${node.name}?workspace_id=${workspaceId}`}
               />
             </TabsContent>
             {role === Roles.Admin && (
