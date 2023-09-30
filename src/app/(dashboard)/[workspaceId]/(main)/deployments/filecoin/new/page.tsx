77893d6ed4864ee9a3d63de806d8fd4513def77c
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-09-30 11:31:04 +0300

    create filecoin node

--- src/app/(dashboard)/[workspaceId]/(main)/deployments/filecoin/new/page.tsx
+++ src/app/(dashboard)/[workspaceId]/(main)/deployments/filecoin/new/page.tsx
@@ -1,12 +1,12 @@
 import { notFound } from "next/navigation";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CreateAptosNodeForm } from "../components/create-aptos-node-form";
+import { CreateNewFilecoinNode } from "../components/create-aptos-node-form";
 import { getClientVersions } from "@/services/get-client-versions";
 import { getWorkspace } from "@/services/get-workspace";
 import { Roles } from "@/enums";
 
-export default async function CreateNewAptosNodePage({
+export default async function CreateNewFilecoinNodePage({
   params,
 }: {
   params: { workspaceId: string };
@@ -17,17 +17,17 @@ export default async function CreateNewAptosNodePage({
   if (role === Roles.Reader) notFound();
 
   const { component } = await getClientVersions({
-    protocol: "aptos",
+    protocol: "filecoin",
     component: "node",
   });
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Create New Aptos Node</CardTitle>
+        <CardTitle>Create New Filecoin Node</CardTitle>
       </CardHeader>
       <CardContent>
-        <CreateAptosNodeForm images={component} />
+        <CreateNewFilecoinNode images={component} />
       </CardContent>
     </Card>
   );
