9d72e57a9d304939e9fcefe8f8f0f1750834a77e
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-10-05 22:19:44 +0300

    networking tab for stacks node

--- src/app/(dashboard)/[workspaceId]/(main)/deployments/stacks/[nodeName]/components/networking-tab.tsx
+++ src/app/(dashboard)/[workspaceId]/(main)/deployments/stacks/[nodeName]/components/networking-tab.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { client } from "@/lib/client-instance";
-import { PolkadotNode, Secret } from "@/types";
+import { Secret, StacksNode } from "@/types";
 import { PolkadotSyncModes, Roles, SecretType } from "@/enums";
 import {
   Form,
@@ -33,24 +33,13 @@ import { getSelectItems } from "@/lib/utils";
 import { Switch } from "@/components/ui/switch";
 
 interface NetWorkingTabProps {
-  node: PolkadotNode;
+  node: StacksNode;
   role: Roles;
   secrets: Secret[];
 }
 
 const schema = z.object({
   nodePrivateKeySecretName: z.string().default(""),
-  p2pPort: z.coerce
-    .number({ invalid_type_error: "P2P Port is number" })
-    .min(1, "P2P Port is between 1 and 65535")
-    .max(65535, "P2P Port is between 1 and 65535")
-    .default(30333),
-  syncMode: z.nativeEnum(PolkadotSyncModes),
-  retainedBlocks: z.coerce
-    .number({ invalid_type_error: "Retained Blocks is number" })
-    .min(1, "Retained Blocks is greater than 0")
-    .default(256),
-  pruning: z.boolean(),
 });
 
 type Schema = z.infer<typeof schema>;
@@ -61,23 +50,11 @@ export const NetworkingTab: React.FC<NetWorkingTabProps> = ({
   secrets,
 }) => {
   const params = useParams();
-  const {
-    nodePrivateKeySecretName,
-    p2pPort,
-    syncMode,
-    retainedBlocks,
-    pruning,
-  } = node;
+  const { nodePrivateKeySecretName } = node;
 
   const form = useForm<Schema>({
     resolver: zodResolver(schema),
-    defaultValues: {
-      nodePrivateKeySecretName,
-      p2pPort,
-      syncMode,
-      retainedBlocks,
-      pruning,
-    },
+    defaultValues: { nodePrivateKeySecretName },
   });
 
   const {
@@ -95,24 +72,12 @@ export const NetworkingTab: React.FC<NetWorkingTabProps> = ({
 
   const onSubmit = async (values: Schema) => {
     try {
-      const { data } = await client.put<PolkadotNode>(
-        `/polkadot/nodes/${node.name}`,
+      const { data } = await client.put<StacksNode>(
+        `/stacks/nodes/${node.name}`,
         values
       );
-      const {
-        nodePrivateKeySecretName,
-        p2pPort,
-        syncMode,
-        retainedBlocks,
-        pruning,
-      } = data;
-      reset({
-        nodePrivateKeySecretName,
-        p2pPort,
-        syncMode,
-        retainedBlocks,
-        pruning,
-      });
+      const { nodePrivateKeySecretName } = data;
+      reset({ nodePrivateKeySecretName });
     } catch (error) {
       if (isAxiosError(error)) {
         const { response } = error;
@@ -159,7 +124,7 @@ export const NetworkingTab: React.FC<NetWorkingTabProps> = ({
                       </SelectItem>
                     ))}
                     <Link
-                      href={`/${params.workspaceId}/secrets/new?type=${SecretType["Polkadot Private Key"]}`}
+                      href={`/${params.workspaceId}/secrets/new?type=${SecretType["Stacks Private Key"]}`}
                       className="text-sm text-primary hover:underline underline-offset-4"
                     >
                       Create New Private Key
@@ -177,88 +142,6 @@ export const NetworkingTab: React.FC<NetWorkingTabProps> = ({
                   </Button>
                 )}
               </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="p2pPort"
-          render={({ field }) => (
-            <FormItem className="max-w-sm">
-              <FormLabel>P2P Port</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={isSubmitting || role === Roles.Reader}
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="syncMode"
-          render={({ field }) => (
-            <FormItem className="max-w-sm">
-              <FormLabel>Sync Mode</FormLabel>
-              <Select
-                disabled={isSubmitting || role === Roles.Reader}
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                value={field.value}
-              >
-                <FormControl>
-                  <SelectTrigger data-testid="network" className="bg-white">
-                    <SelectValue />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {getSelectItems(PolkadotSyncModes).map(({ value, label }) => (
-                    <SelectItem key={value} value={value}>
-                      {label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="pruning"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center gap-x-3">
-              <FormLabel className="mt-2">Pruning</FormLabel>
-              <FormControl>
-                <Switch
-                  disabled
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="retainedBlocks"
-          render={({ field }) => (
-            <FormItem className="max-w-sm">
-              <FormLabel>Retained Blocks</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={isSubmitting || role === Roles.Reader}
-                  {...field}
-                />
-              </FormControl>
               <FormMessage />
             </FormItem>
           )}
