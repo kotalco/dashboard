d1379538b6aec651844ddc03b9eb7560f75e6e62
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-10-08 10:56:32 +0300

    create endpoints

---
+++ src/app/(dashboard)/[workspaceId]/(main)/endpoints/new/page.tsx
@@ -0,0 +1,49 @@
+import { notFound } from "next/navigation";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CreateEndpointForm } from "../components/create-endpoint-form";
+import { getWorkspace } from "@/services/get-workspace";
+import { Roles } from "@/enums";
+import { getDomainInfo } from "@/services/get-domain-info";
+import { getServices } from "@/services/get-services";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import Link from "next/link";
+
+export default async function CreateNewAptosNodePage({
+  params,
+}: {
+  params: { workspaceId: string };
+}) {
+  const { workspaceId } = params;
+  const { name } = await getDomainInfo();
+  const { services } = await getServices(workspaceId);
+  const { role } = await getWorkspace(workspaceId);
+
+  if (role === Roles.Reader) notFound();
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create New Endpoint</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {!name && (
+          <Alert variant="info">
+            <AlertDescription>
+              Your domain is not configured yet. Click{" "}
+              <Link
+                className="text-primary hover:underline underline-offset-4"
+                href={`/${workspaceId}/domain`}
+              >
+                here
+              </Link>{" "}
+              to set your custom domain
+            </AlertDescription>
+          </Alert>
+        )}
+
+        {name && <CreateEndpointForm services={services} />}
+      </CardContent>
+    </Card>
+  );
+}
