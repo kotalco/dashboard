42d36691991312798f5473705f747663a635b31c
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-11-14 09:10:19 +0300

    show alert in case of subscription is past due, move credit balance to the top

--- src/app/(managed-dashboard)/(main)/billing/plan/_components/manage-plan-card.tsx
+++ src/app/(managed-dashboard)/(main)/billing/plan/_components/manage-plan-card.tsx
@@ -18,6 +18,7 @@ import { UserCreditBalance } from "./user-credit-balance";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn, getEnumKey } from "@/lib/utils";
 import { SubscriptionStatus } from "@/enums";
+import { Alert } from "@/components/ui/alert";
 
 export const ManagePlanCard = async () => {
   const supscriptionPromise = getCurrentSubscription();
@@ -29,40 +30,49 @@ export const ManagePlanCard = async () => {
   ]);
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          {subscription.plan.name} Plan{" "}
-          <span
-            className={cn("text-sm font-semibold tracking-wider", {
-              "text-green-600":
-                subscription.status === SubscriptionStatus.Active,
-              "text-destructive":
-                subscription.status !== SubscriptionStatus.Active,
-            })}
-          >
-            {getEnumKey(SubscriptionStatus, subscription.status)}
-          </span>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <PlanDetails />
-      </CardContent>
-      <CardFooter className="flex justify-between gap-x-2">
-        <Suspense fallback={<Skeleton className="w-[200px] h-10" />}>
-          <UserCreditBalance />
-        </Suspense>
-        {!subscription.canceled_at && (
-          <div className="space-x-4">
-            <ChangePlan plans={plans} />
-            <CancelPlan subscriptionId={subscription.id} />
-          </div>
-        )}
+    <div className="space-y-3">
+      <Suspense fallback={<Skeleton className="w-[200px] h-10" />}>
+        <UserCreditBalance />
+      </Suspense>
+      {subscription.status === SubscriptionStatus["Past Due"] && (
+        <Alert variant="destructive" className="text-center">
+          Your last payment attempt has been failed. You can complete your
+          payment by clicking on <strong>continue your payment</strong> from
+          invoices section.
+        </Alert>
+      )}
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            {subscription.plan.name} Plan{" "}
+            <span
+              className={cn("text-sm font-semibold tracking-wider", {
+                "text-green-600":
+                  subscription.status === SubscriptionStatus.Active,
+                "text-destructive":
+                  subscription.status !== SubscriptionStatus.Active,
+              })}
+            >
+              {getEnumKey(SubscriptionStatus, subscription.status)}
+            </span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <PlanDetails />
+        </CardContent>
+        <CardFooter className="flex justify-end gap-x-2">
+          {!subscription.canceled_at && (
+            <div className="space-x-4">
+              <ChangePlan plans={plans} />
+              <CancelPlan subscriptionId={subscription.id} />
+            </div>
+          )}
 
-        {!!subscription.canceled_at && (
-          <ReactivatePlan subscriptionId={subscription.id} />
-        )}
-      </CardFooter>
-    </Card>
+          {!!subscription.canceled_at && (
+            <ReactivatePlan subscriptionId={subscription.id} />
+          )}
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
