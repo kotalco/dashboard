b6fa89859dd32d807a8f9ba00aecbf88438d8a6a
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-12-01 22:09:06 +0300

    server actions for reset password

--- src/app/(auth)/(routes)/reset-password/page.tsx
+++ src/app/(auth)/(routes)/reset-password/page.tsx
@@ -1,7 +1,16 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ResetPasswordForm } from "./components/reset-password-form";
+import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { token: string; email: string };
+}) {
+  if (!searchParams.token || !searchParams.email) {
+    redirect("/");
+  }
+
   return (
     <>
       <Card>
