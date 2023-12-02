421771ad96af5d24f13aa52c44bf27464d1d010c
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-12-02 03:34:50 +0300

    server action for delete secret

--- src/components/ui/close-dialog-button.tsx
+++ src/components/ui/close-dialog-button.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 import { DialogClose, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
@@ -6,10 +10,11 @@ interface CloseDialogButtonProps {
 }
 
 export const CloseDialogButton = ({ children }: CloseDialogButtonProps) => {
+  const { pending } = useFormStatus();
   return (
     <DialogFooter className="sm:justify-end">
       <DialogClose asChild>
-        <Button type="button" variant="outline">
+        <Button disabled={pending} type="button" variant="outline">
           Cancel
         </Button>
       </DialogClose>
