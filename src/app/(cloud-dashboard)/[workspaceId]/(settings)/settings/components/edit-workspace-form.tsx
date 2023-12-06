43b1c54799189e3c18c99e40a6bb22510c1ee7b3
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-12-06 09:22:18 +0300

    remove unused code

--- src/app/(cloud-dashboard)/[workspaceId]/(settings)/settings/components/edit-workspace-form.tsx
+++ src/app/(cloud-dashboard)/[workspaceId]/(settings)/settings/components/edit-workspace-form.tsx
@@ -1,35 +1,13 @@
 "use client";
 
-import * as z from "zod";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { isAxiosError } from "axios";
-
-import { client } from "@/lib/client-instance";
-import { User, Workspace } from "@/types";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/form/input";
-import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Workspace } from "@/types";
 import { useAction } from "@/hooks/use-action";
 import { editWorkspace } from "@/actions/edit-workspace";
+
+import { Input } from "@/components/form/input";
 import { SubmitError } from "@/components/form/submit-error";
 import { SubmitSuccess } from "@/components/form/submit-success";
 import { SubmitButton } from "@/components/form/submit-button";
-
-const schema = z.object({
-  name: z.string().min(3, "Workspace name must be 3 characters at least"),
-});
-
-type SchemaType = z.infer<typeof schema>;
 
 interface EditWorkspaceFormProps {
   workspace: Workspace;
@@ -40,23 +18,6 @@ export const EditWorkspaceForm: React.FC<EditWorkspaceFormProps> = ({
 }) => {
   const { name, id } = workspace;
   const { execute, error, success, fieldErrors } = useAction(editWorkspace);
-  const router = useRouter();
-  const form = useForm<SchemaType>({
-    resolver: zodResolver(schema),
-    defaultValues: { name },
-  });
-
-  const {
-    formState: {
-      isSubmitted,
-      isSubmitting,
-      isValid,
-      isDirty,
-      isSubmitSuccessful,
-      errors,
-    },
-    setError,
-  } = form;
 
   const onSubmit = (formData: FormData) => {
     const name = formData.get("name") as string;
