6e2262ac30ec1fd077f91da249e7b81a78e31bb4
ahmed-abdelhamid <slevin.glevra@gmail.com>
2023-12-17 19:38:53 +0300

    create getBaseUrl to get the domain and protcol

--- src/lib/server-instance.ts
+++ src/lib/server-instance.ts
@@ -5,9 +5,25 @@ import axios, {
   AxiosResponse,
   InternalAxiosRequestConfig,
 } from "axios";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 import { StorageItems } from "@/enums";
+
+// if base url is absolute path, convert it to full url
+// exmaple: /api/v1 will be converted to http://domain/api/v1
+export const getBaseURL = () => {
+  let url = process.env.API_URL;
+
+  if (url?.startsWith("/")) {
+    const headersList = headers();
+    const protocol = headersList.get("x-forwarded-proto");
+    const domain = headersList.get("host");
+
+    url = `${protocol}://${domain}${url}`;
+  }
+
+  return url;
+};
 
 interface CustomAxiosResponse extends AxiosResponse {
   config: InternalAxiosRequestConfig & {
@@ -30,15 +46,15 @@ const getCacheKey = (config: AxiosRequestConfig): string => {
   return `${config.method}:${config.url}`;
 };
 
-export const server = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+export const server = axios.create();
 
 server.interceptors.request.use((config) => {
-  console.log("Base URL: ", config.baseURL);
-  console.log("URL: ", config.url);
   const key = getCacheKey(config);
   const now = Date.now();
+  config.baseURL = getBaseURL();
+
+  console.log("Base URL: ", config.baseURL);
+  console.log("URL: ", config.url);
 
   const token = cookies().get(StorageItems.AUTH_TOKEN);
   if (token?.value) config.headers.Authorization = `Bearer ${token.value}`;
