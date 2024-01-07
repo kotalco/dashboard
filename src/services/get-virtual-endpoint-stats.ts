ca8db0533b6d9de0620b57b39a0bb0ace077699c
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-01-07 13:56:26 +0300

    remove delay for virtual endpoint by mistake

--- src/services/get-virtual-endpoint-stats.ts
+++ src/services/get-virtual-endpoint-stats.ts
@@ -4,11 +4,10 @@ import { unstable_noStore as noStore } from "next/cache";
 
 import { server } from "@/lib/server-instance";
 import { EndpointStats } from "@/types";
-import { delay, logger } from "@/lib/utils";
+import { logger } from "@/lib/utils";
 
 export const getVirtualEndpointStats = async (name: string) => {
   noStore();
-  await delay(5000);
   try {
     const response = await server.get<EndpointStats>(
       `/virtual-endpoints/${name}/stats`
