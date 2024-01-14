bc593dbb78bf2d0b2355eeda555a895e1ef49de6
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-01-14 18:20:47 +0300

    fix logo redirect to home page

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { Logo } from "@/components/logo";
 import { findUser } from "@/services/find-user";
 import Workspaces from "@/components/workspaces";
+import Link from "next/link";
 
 export const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   const { user } = await findUser();
@@ -11,7 +12,9 @@ export const Sidebar = async ({ children }: { children: React.ReactNode }) => {
     <div className="flex flex-col w-64 fixed inset-y-0 left-0 overflow-y-auto px-3 py-4 space-y-4 border-r">
       <div className="flex-1 flex flex-col">
         <div>
-          <Logo />
+          <Link href="/">
+            <Logo />
+          </Link>
         </div>
         <div className="pt-4 flex flex-col flex-1">
           <nav className="flex flex-col flex-1">
