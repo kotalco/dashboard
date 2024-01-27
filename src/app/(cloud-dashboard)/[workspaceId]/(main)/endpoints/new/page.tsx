d908a42776f45578d00231fc05e78b79a7c6e21d
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-01-28 01:15:20 +0300

    ux: create endpoint, secrets pages

--- src/app/(cloud-dashboard)/[workspaceId]/(main)/endpoints/new/page.tsx
+++ src/app/(cloud-dashboard)/[workspaceId]/(main)/endpoints/new/page.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CreateEndpointForm } from "../_components/create-endpoint-form";
 import { getWorkspace } from "@/services/get-workspace";
 import { Roles } from "@/enums";
 import { getDomainInfo } from "@/services/get-domain-info";
 import { getServices } from "@/services/get-services";
+
 import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Heading } from "@/components/ui/heading";
+
+import { CreateEndpointForm } from "./_components/create-endpoint-form";
 
 export default async function CreateNewAptosNodePage({
   params,
@@ -22,28 +24,24 @@ export default async function CreateNewAptosNodePage({
   if (role === Roles.Reader) notFound();
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Create New Endpoint</CardTitle>
-      </CardHeader>
-      <CardContent>
-        {!name && (
-          <Alert className="alert-info">
-            <AlertDescription>
-              Your domain is not configured yet. Click{" "}
-              <Link
-                className="text-primary hover:underline underline-offset-4"
-                href={`/${workspaceId}/domain`}
-              >
-                here
-              </Link>{" "}
-              to set your custom domain
-            </AlertDescription>
-          </Alert>
-        )}
+    <div className="space-y-8">
+      <Heading title="New Endpoint" />
+      {!name && (
+        <Alert className="alert-info">
+          <AlertDescription>
+            Your domain is not configured yet. Click{" "}
+            <Link
+              className="text-primary hover:underline underline-offset-4"
+              href={`/${workspaceId}/domain`}
+            >
+              here
+            </Link>{" "}
+            to set your custom domain
+          </AlertDescription>
+        </Alert>
+      )}
 
-        {name && <CreateEndpointForm services={services} />}
-      </CardContent>
-    </Card>
+      {name && <CreateEndpointForm services={services} />}
+    </div>
   );
 }
