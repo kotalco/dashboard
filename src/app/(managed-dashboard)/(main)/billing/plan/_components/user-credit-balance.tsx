83ebf874a480091df328eda28377cf7ce50605ab
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-02-14 02:31:39 +0300

    add dialog to credit balance

--- src/app/(managed-dashboard)/(main)/billing/plan/_components/user-credit-balance.tsx
+++ src/app/(managed-dashboard)/(main)/billing/plan/_components/user-credit-balance.tsx
@@ -1,9 +1,14 @@
-import { Wallet } from "lucide-react";
-
 import { formatCurrency } from "@/lib/utils";
 import { getUserCredit } from "@/services/get-user-credit";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export const UserCreditBalance = async () => {
   const { creditBalance } = await getUserCredit();
@@ -11,12 +16,29 @@ export const UserCreditBalance = async () => {
   if (creditBalance?.balance) {
     return (
       <div className="flex justify-end">
-        <div className="flex justify-between w-full max-w-[200px] px-3 py-2 text-xs leading-6 rounded-xl bg-primary/5 text-foreground">
-          <span className="flex items-center space-x-2">
-            <span>Account Credit</span>
-          </span>
-          <span>{formatCurrency(Math.abs(creditBalance.balance) || 0)}</span>
-        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              className="max-w-[200px] w-full text-muted-foreground"
+              variant="outline"
+            >
+              <div className="w-full flex justify-between">
+                <span>Account Credit</span>
+                <span>
+                  {formatCurrency(Math.abs(creditBalance.balance) || 0)}
+                </span>
+              </div>
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader />
+            <div className="text-muted-foreground">
+              Account credit is a remaining balance after downgrading from a
+              plan to another of less price. Account credit can be used in
+              future payments.
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     );
   }
