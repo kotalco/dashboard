c3bd091548ad0daecdfd319eac395aa0211bf372
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-02-14 02:17:02 +0300

    rename credit to account credit

--- src/app/(managed-dashboard)/(main)/billing/plan/_components/user-credit-balance.tsx
+++ src/app/(managed-dashboard)/(main)/billing/plan/_components/user-credit-balance.tsx
@@ -13,7 +13,7 @@ export const UserCreditBalance = async () => {
       <div className="flex justify-end">
         <div className="flex justify-between w-full max-w-[200px] px-3 py-2 text-xs leading-6 rounded-xl bg-primary/5 text-foreground">
           <span className="flex items-center space-x-2">
-            <span>Credit</span>
+            <span>Account Credit</span>
           </span>
           <span>{formatCurrency(Math.abs(creditBalance.balance) || 0)}</span>
         </div>
