bf4e8c34b1073c7bed748449ccdb89b1effe46c3
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-02-14 19:46:04 +0300

    refactor my cards page

--- src/app/(managed-dashboard)/(settings)/payment-methods/page.tsx
+++ src/app/(managed-dashboard)/(settings)/payment-methods/page.tsx
@@ -1,45 +1,19 @@
+import { Suspense } from "react";
+
 import { Heading } from "@/components/ui/heading";
-import { getPaymentMethods } from "@/services/get-payment-methods";
-import { CreditCard } from "lucide-react";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { CardsList } from "./_components/cards-list";
-import { AddPaymentCard } from "./_components/add-payment-card";
+
+import { CardListSkeleton, CardsList } from "./_components/cards-list";
 
 export default async function PaymentMethodsPage() {
-  const { cards } = await getPaymentMethods();
-
   return (
-    <div className="flex-col">
-      <div className="flex-1 p-8 pt-6 space-y-4">
-        <div className="flex items-center justify-between">
-          <Heading title="My Cards" />
-
-          {!!cards.length && <AddPaymentCard />}
-        </div>
-
-        <CardsList cards={cards} />
-        {!cards.length && (
-          <Card className="flex flex-col items-center text-center">
-            <CardHeader className="flex flex-col items-center">
-              <CreditCard className="w-16 h-16" />
-              <CardTitle>There is no any payment cards added yet!</CardTitle>
-              <CardDescription>
-                Add your card here to be able to create your subscription.
-              </CardDescription>
-            </CardHeader>
-
-            <CardFooter>
-              <AddPaymentCard />
-            </CardFooter>
-          </Card>
-        )}
+    <div className="grid grid-cols-12 items-center gap-8 pr-10">
+      <div className="col-span-12 md:col-span-7 lg:col-span-8 xl:col-span-9">
+        <Heading title="My Cards" />
       </div>
+
+      <Suspense fallback={<CardListSkeleton />}>
+        <CardsList />
+      </Suspense>
     </div>
   );
 }
